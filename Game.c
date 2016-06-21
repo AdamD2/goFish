eff193c4970ac030dc6d93b43f05766b7f92fc6f
jimmystumps <beltofdeltora@gmail.com>
2016-06-22 00:07:20 +1000

    Removed findPairs, only stub left

--- Game.c
+++ Game.c
@@ -207,52 +207,6 @@ int checkPlayer (Game g, action a) {
 }
 
 void findPairs (Game g) {
-    list hand = g->playerArray[g->whoseTurn-1].playerHand;
-    link card = hand->head;
-    //Holds the first matching card of a set
-    link headCard = hand->head;
-    //Used in the inner loop so as to not interfere with the flow
-    //of the outer loop
-    link pairCard = NULL;
-    int cardVal;
-    int same = TRUE;
-    int count = 0;
-    int looped;
-
-    bubbleSort (hand);
-
-    if (listLength (hand) >= 4) {
-        //Outer loop to find each set of 4
-        while (card->next != NULL) {
-            looped = FALSE;
-            headCard = card;
-            cardVal = card->value;
-
-            //Inner loop to check the next three values
-            pairCard = card;
-            while (same == TRUE && count < PAIR_SIZE &&
-            pairCard->next != NULL) {
-                looped = TRUE;
-                pairCard = pairCard->next;
-                if (cardVal == card->value) {
-                    same = TRUE;
-                    count++;
-                } else {
-                    same = FALSE;
-                }
-            }
-
-            if (count == PAIR_SIZE) {
-                removePair (g, headCard);
-            }
-            
-            if(looped) {
-                card = pairCard;
-            } else {
-                card = pairCard->next;
-            }
-        }
-    }
 }
 
 static void removePair (Game g, link headCard) {
