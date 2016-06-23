01329bb17af399fa03c414792c11b2c3723120d8
AdamD2 <adam_douglas2@hotmail.com>
2016-06-23 12:52:44 +1000

    Added an AI file/stub to build upon.

---
+++ ai.c
@@ -0,0 +1,17 @@
+/* Name: Go Fish AI Player
+ * Creators: Adam Douglas, James Wardman
+ * Date: 23-06-2016
+ */
+
+#include "Game.h"
+
+action decideAction (Game g);
+
+action decideAction (Game g) {
+    action nextAction;
+    int me = getWhoseTurn (g);
+
+    
+
+    return nextAction;
+}
