b5b606e596624e621a264acf3538e5d5843caca7
AdamD2 <adam_douglas2@hotmail.com>
2016-06-28 17:09:25 +1000

    Fixed testGame so that it can use the game struct. I have also added a print statement to signify that the findPairs function is being tested and moved the array indexing from 1 down to 0.

--- testGame.c
+++ testGame.c
@@ -9,12 +9,21 @@ void testFindPairs ();
 list newList ();
 void appendInt (list l, int val);
 
-int main (int argc, char* argv[]) {
-    
+typedef struct _player {
+    list playerHand;
+    int pairs;
+} player;
 
+typedef struct _game {
+    list deck;
+    player playerArray[NUM_PLAYERS];
+    int whoseTurn;
+    int roundNumber;
+} game;
+
+int main (int argc, char* argv[]) {
 
     testFindPairs();
-
 
     printf ("All tests passed you are awesome!\n");
 
@@ -22,26 +31,28 @@ int main (int argc, char* argv[]) {
 }
 
 void testFindPairs () {
-    int testArray1[6] = {1, 2, 3, 4, 5, 6};
-    int testArray2[17] = {4, 4, 4, 4, 5, 6, 7, 7, 8, 8, 8, 10, 12, 12,
+    int testArray0[6] = {1, 2, 3, 4, 5, 6};
+    int testArray1[17] = {4, 4, 4, 4, 5, 6, 7, 7, 8, 8, 8, 10, 12, 12,
                          12, 13, 13};
     
+    // Create the game struct for testing
     Game g = newGame ();
 
     list test = newList();
     g->playerArray[0].playerHand = test;
 
+    printf ("Testing findPairs.\n");
 
-    printf ("%d\n", testArray2[0]);
+    printf ("%d\n", testArray1[0]);
 
     for (int i = 0; i < 6; i++) {
-        appendInt (test, testArray1[i]);
+        appendInt (test, testArray0[i]);
     }
 
     assert (test->head->value == 1);
     //findPairs (g);
 
-    printf ("findPairs works\n");
+    printf ("findPairs tests passed.\n");
 }
 
 list newList () {
